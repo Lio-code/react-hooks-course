b45763365c74b69dbf90fece4389ee2ab7a4dab0
lionel <lio4455@hotmail.com>
2022-08-15 12:32:19 +0200

    docs: personal comment for useCallback hook

--- src/UseCallback/CallbackTutorial.js
+++ src/UseCallback/CallbackTutorial.js
@@ -2,6 +2,12 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import Child from "./Child";
 
+//useCallback hook est similaire à useMemo, dans le sens où il permet aussi d'éviter le calcul inutile d'une fonction, ou de limiter son application en spécifiant une dépendance.
+//Contrairement à useMemo qui stocke une valeur de retour, useCallback stocke une fonction en retour.
+
+
+
+
 export default function CallBackTutorial() {
   const [toggle, setToggle] = useState(false);
   const [data, setData] = useState("Yo, pls sub to the channel!");
